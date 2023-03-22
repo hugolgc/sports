61e8b79ad50137951c9ecbea979943b638ac3cf6
hugolgc <lagache.hugo@hotmail.fr>
2023-03-22 11:53:30 +0100

    🚀 Started

---
+++ src/app/app.component.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template></template>
+
+<style scoped lang="scss">
+@import "./app.component";
+</style>
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createApp } from "vue";
+import AppComponent from "./app/app.component.vue";
+import "./assets/style.scss";
+
+createApp(AppComponent).mount("#sports");
